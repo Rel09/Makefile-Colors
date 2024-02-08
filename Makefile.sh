c84b2948ab521f2db3bfad039acd3a809fa5a3d3
Rel09 <davkojak@hotmail.com>
2024-02-08 10:09:08 -0500

    Changed how it actually works

--- Makefile.sh
+++ Makefile.sh
@@ -25,23 +25,6 @@
 
 #  Now you can literally use that inside your makefile
 
-
-# ------------------------------------------------ #
-#                  Color's Define
-# ------------------------------------------------ #
-
-# Forecolor
-for ((i = 0; i < 256; i++)); do
-    printf -v FG_COLOR_256_$i '\033[38;5;%dm' $i
-    eval "colorText_$i() { echo -e \"\${FG_COLOR_256_$i}\$1\033[0m\"; }"
-done
-
-# Background
-for ((i = 0; i < 256; i++)); do
-    printf -v BG_COLOR_256_$i '\033[48;5;%dm' $i
-    eval "text_bg_color_$i() { echo -e \"\${BG_COLOR_256_$i}\$1\033[0m\"; }"
-done
-
 # ------------------------------------------------ #
 #               Makefile's Functions
 # ------------------------------------------------ #
@@ -51,7 +34,7 @@ colorTxt() {
     local index=$1
     shift
     local text="$*"
-    eval "colorText_$index \"$text\033[0m\""
+    echo -e "\033[38;5;${index}m$text\033[0m"
 }
 
 #   ./Makefile.sh colorBg [Text_Color_Index] [Background_Color_Index] [Text]
@@ -60,14 +43,14 @@ colorBg() {
     local bg_index=$2
     shift 2
     local text="$*"
-    eval "colorText_$fg_index \"\$(text_bg_color_$bg_index \"$text\033[0m\")\""
+    echo -e "\033[38;5;${fg_index}m\033[48;5;${bg_index}m$text\033[0m"
 }
 
 # ------------------------------------------------ #
 #               Display all the colors
 # ------------------------------------------------ #
 printAllColors() {
-    for ((i = 0; i < 256; i++)); do
+    for ((i = 0; i < 256; i++)); do 
         printf "\033[38;5;%dm Color %-3d \033[0m " "$i" "$i"
         if (( (i + 1) % 10 == 0 )); then
             printf "\n"
@@ -87,6 +70,19 @@ printAllBackgroundColors() {
 }
 
 printAll() {
+
+    # Forecolor
+    for ((i = 0; i < 256; i++)); do
+        printf -v FG_COLOR_256_$i '\033[38;5;%dm' $i
+        eval "colorText_$i() { echo -e \"\${FG_COLOR_256_$i}\$1\033[0m\"; }"
+    done
+
+    # Background
+    for ((i = 0; i < 256; i++)); do
+        printf -v BG_COLOR_256_$i '\033[48;5;%dm' $i
+        eval "text_bg_color_$i() { echo -e \"\${BG_COLOR_256_$i}\$1\033[0m\"; }"
+    done
+
     printAllColors
     printAllBackgroundColors
 }
